7258c4a03adf55d4f18b5db9968f3814382876de
ALi <s.arme@ya.ru>
2020-03-24 23:06:45 +0430

    Initialize variables in de_button_game/main.c

--- de_button_game/main.c
+++ de_button_game/main.c
@@ -8,10 +8,26 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-int Pressed_Confidence_level;
+int Pressed_Confidence_level[2];
+int Released_Confidence_level[2];
+int Pressed[2]; 
+// which LED is being led up
+int LEDNumber[2]
+
 int main(void)
 {
     /* Initialization area. Setting up ports and data direction registers */
+	// control inputs  PC0, PC1
+	// set DD to 0 for inputs
+	DDRC &= ~(1 << DDC0);
+	DDRC &= ~(1 << DDC1);
+	// set for high reading
+	PORTC |= (1 << DDC0);
+	PORTC |= (1 << DDC1);
+	// DD to 1 for output
+	DDRB = -1;
+	DDRD |= ~(3 << 6);
+	 
     while (1)
 	{
 		if (/*button pressed for side 1*/) {
